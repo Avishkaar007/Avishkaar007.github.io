b3dca7dd3ae7f5cf5b44fffb1510f09186f81c79
Avishkaar Pawar <63647102+Avishkaar007@users.noreply.github.com>
2023-08-08 00:43:22 +0530

    hamming-0.2.0

--- Projects/VLab/hamming-code/src/App.js
+++ Projects/VLab/hamming-code/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <div class="logo"><img class="logo" src='/public/images/logo.png'/></div>
-        <h1>V-Lab @ ANDC</h1>
-        <ul class="navigation">
-            <li><a href="https://www.vlab.andcollege.du.ac.in/index.html">Home</a></li>
-            <li><a href="https://www.vlab.andcollege.du.ac.in/index.html#labs_section">Labs</a></li>
-            <li><a href="https://www.vlab.andcollege.du.ac.in/index.html#team">Team</a></li>
-            <li><a href="https://www.andcollege.du.ac.in/" target="_blank" rel="noopener noreferrer">College
-                    Website</a>
-            </li>
-        </ul>
-    </header>
+import Header from "./webpages/Header.js";
+import Nav from "./webpages/Nav.js";
+import Container from "./webpages/Container.js";
+import "./App.css";
+function App(){
+  return(
+    <>
+    <Header />
+    <div class="exp-heading">
+      <h1>Hamming Code</h1>
     </div>
-  );
-}
+    <Nav/>
+    <Container/>
+    </>    
 
+  )
+}
 export default App;
